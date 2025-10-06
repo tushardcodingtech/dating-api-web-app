ae66213a2751d3dd121587a08f071a75254deb06
Tushar <tushardcodingtech@gmail.com>
2025-10-06 12:00:58 +0530

    minor changes

--- seedGifts.js
+++ seedGifts.js
@@ -10,9 +10,9 @@ mongoose.connect(MONGODB_URI, {
 });
 
 const gifts = [
-  { name: "Teddy Bear", price: 100, image: "https://example.com/teddy.png" },
-  { name: "Roses", price: 50, image: "https://example.com/roses.png" },
-  { name: "Chocolate Box", price: 75, image: "https://example.com/chocolate.png" },
+  { name: "Teddy Bear", price: 100, image: "https://as1.ftcdn.net/jpg/13/88/70/76/1000_F_1388707699_xgAtGl9NjYuibhGjTGgKcAKUVa3of26t.jpg" },
+  { name: "Roses", price: 50, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Rose_flower.jpg/1200px-Rose_flower.jpg?20130228001944" },
+  { name: "Chocolate Box", price: 75, image: "https://www.shutterstock.com/shutterstock/photos/2420286489/display_1500/stock-vector-hand-drawn-vector-illustration-of-a-heart-shaped-chocolate-candy-gift-box-sweet-valentine-s-day-2420286489.jpg" },
 ];
 
 async function seed() {
