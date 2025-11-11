49a397f9e874da22a5cb1bf41f485114d829293f
Tushar <tushardcodingtech@gmail.com>
2025-11-11 14:35:12 +0530

    minor change

--- models/Category.js
+++ models/Category.js
@@ -9,5 +9,4 @@ const categorySchema = new mongoose.Schema({
   image: { type: String },
 }, { timestamps: true });
 
-const Category = mongoose.model("Category", categorySchema);
-module.exports = Category;
+module.exports = mongoose.model("Category", categorySchema);
