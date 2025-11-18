51150f07af31d5c84146b90111d4ab834b9e0b85
Tushar <tushardcodingtech@gmail.com>
2025-11-18 16:04:28 +0530

    create chat model

---
+++ models/Chat.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    messages: [
+      {
+        sender: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Chat", chatSchema);
